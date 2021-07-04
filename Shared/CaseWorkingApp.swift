77c93a38248d9bb79b9c392e7471342bef56d933
David Okun <david@okun.io>
2021-07-04 18:47:19 -0500

    Implemented searchable - wow, that was fast

--- Shared/CaseWorkingApp.swift
+++ Shared/CaseWorkingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CaseWorkingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          ContentView(children: try! Helpers.getMockChildren())
         }
     }
 }
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,23 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-  var mockItems = try! Helpers.getMockChildren()
+  @State var searchText = ""
+  @State var children: [Child]
   var body: some View {
     NavigationView {
-      List(mockItems) { child in
+      List(children) { child in
         NavigationLink(
           destination: ChildDetailView(child: child),
           label: {
-            ChildCell(child: child)
+          ChildCell(child: child)
         }).isDetailLink(true)
       }.listStyle(InsetGroupedListStyle())
-      .navigationBarTitle("Children")
+        .navigationBarTitle("Children")
+        .searchable(text: $searchText)
+        .onChange(of: searchText) { searchText in
+          if !searchText.isEmpty {
+            children = try! Helpers.getMockChildren().filter { $0.firstName.contains(searchText) || $0.lastName.contains(searchText) }
+          } else {
+            children = try! Helpers.getMockChildren()
+          }
+        }
     }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(children: try! Helpers.getMockChildren())
   }
 }
